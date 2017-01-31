8726761e72d567252a94d0a525845e6438756ccc
PhilippeGuerin <p.guerin@castsoftware.com>
2017-01-31 17:11:12 -0500

    Retrieve latest version

---
+++ Gorgon Backup/sonatype_2016_12_20_15_54_22.sql
@@ -0,0 +1,106 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = gorgon, pg_catalog;
+
+--
+-- Data for Name: sonatype; Type: TABLE DATA; Schema: gorgon; Owner: operator
+--
+
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('jar', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/target/dependency', 'junit-4.12.jar');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('jar', 'moderate', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-core/target/dependency', 'junit-4.12.jar');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('jar', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/target/dependency', 'hamcrest-core-1.1.jar');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('jar', 'moderate', 'wro4j-1.8.x.zip/wro4j-1.8.x/target/dependency', 'junit-4.12.jar');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('jar', 'moderate', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-core/target/dependency', 'jsp-api-2.0.jar');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('jar', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-core/target/dependency', 'jsp-api-2.0.jar');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('jar', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-core/target/dependency', 'junit-4.12.jar');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('jar', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-core/target/dependency', 'jsp-api-2.0.jar');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('jar', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-core/target/dependency', 'hamcrest-core-1.1.jar');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('jar', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-core/target/dependency', 'servlet-api-2.3.jar');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'deployJava.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expectedReservedNames', 'Reporter.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor', 'less.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'ifreturn.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/testReservedNames', 'Reporter.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'issue9.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'const.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'array1.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-core/src/test/resources/ro/isdc/wro/model/factory/expander/order', '02-xyc.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'issue13.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-core/src/test/resources/ro/isdc/wro/model/factory/expander/subfolder/folder2', 'script2.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-core/src/test/resources/ro/isdc/wro/model/factory/expander', 'script2.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/copyright/test', 'input.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'input.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'issue28.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue10.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'forstatement.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue34.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'array4.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'issue53.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue17.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'empty-blocks.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue20.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'issue29.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'issue34.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'issue30.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'array2.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/google/expectedWhitespaceOnly', 'jquery-1.5.2.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/google/expectedWhitespaceOnly', 'deployJava.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'issue4.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue4.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'if.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'var.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue48.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'issue16.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue54.1.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue16.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'assignment.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'escapedSlash.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'issue20.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'concatstring.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'issue21.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'issue27.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-core/src/test/resources/ro/isdc/wro/model/resource/processor/jsmin/test', 'jquery-1.11.0.min.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue68.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'issue48.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'var.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'array3.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'assignment.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'concatstring.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue14.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/google/expectedSimple', 'jquery-1.5.2.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/google/expectedSimple', 'issue25.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue29.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/google/expectedWhitespaceOnly', 'input.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue30.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/hoganjs', 'hogan-2.0.0.min.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/less', 'init.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue25.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue13.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue69.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/dojo/expected', 'issue54.1.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue50.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'issue69.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'issue11.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'forstatement.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue21.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue27.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'const.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue9.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue53.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue28.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/test', 'issue11.js');
+INSERT INTO sonatype (type, icense_treath_level, pathname_1, pathname_2) VALUES ('js', 'severe', 'wro4j-1.8.x.zip/wro4j-1.8.x/wro4j-extensions/src/test/resources/ro/isdc/wro/extensions/processor/uglify/expected', 'if.js');
+
+
+--
+-- PostgreSQL database dump complete
+--
+
